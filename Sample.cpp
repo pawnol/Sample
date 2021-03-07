9180f5d28a4c0daa9abb76a3c63f770b47958865
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2021-03-07 15:36:27 -0600

    Add files via upload

---
+++ Sample.cpp
@@ -0,0 +1,15 @@
+/*
+Sample
+Pawelski
+3/7/2021
+This is a sample program to practice extracting a zipped folder.
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Congradulations!\n";
+    std::cout << "You just extracted a zipped file.\n";
+    return 0;
+}
